edafd31fec710b18a01fe6b88fe4b283c77dc868
Nat Russell <russ@copirite.com.au>
2012-02-02 11:32:00 +1000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '112d904bb9d16afd067af03a75ba45d51f32ba15995c82c4fc2d38aaf95b4561e44a2e6e39d8bc654d4415300a20164d779f3eab422051fd8e931b53da0e6167'

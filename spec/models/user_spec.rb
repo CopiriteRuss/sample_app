48827d75def4f6266daa5313795db16df6b3c874
Nat Russell <russ@copirite.com.au>
2012-02-28 11:57:46 +1000

    Added password and encrypted_password attributes to the User model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,12 @@ require 'spec_helper'
 describe User do
   
   before(:each) do
-    @attr = { :name => "Example User", :email => "user@example.com"}
+    @attr = {
+      :name => "Example User",
+      :email => "user@example.com",
+      :password => "foobar",
+      :password_confirmation => "foobar"
+    }
   end
 
   it "should create a new instance given a valid attribute" do
@@ -16,8 +21,8 @@ describe User do
   end
   
   it "should require an email address" do
-    no_name_user = User.new(@attr.merge(:email => ""))
-    no_name_user.should_not be_valid
+    no_email_user = User.new(@attr.merge(:email => ""))
+    no_email_user.should_not be_valid
   end
   
   it "should reject names that are too long" do
@@ -25,20 +30,23 @@ describe User do
     long_name_user = User.new(@attr.merge(:name => long_name))
     long_name_user.should_not be_valid
   end
-  
+    
   it "should accept valid email addresses" do
-    addresses = %w[user@foo.com THE_USER@foo.bar.org first.last@foo.jp]
-    addresses.each do |address|
-      valid_email_user = User.new(@attr.merge(:email => address))
-      valid_email_user.should be_valid
-  end
+      addresses = %w[user@foo.com THE_USER@foo.bar.org first.last@foo.jp]
+      addresses.each do |address|
+        valid_email_user = User.new(@attr.merge(:email => address))
+        valid_email_user.should be_valid
+      end
+    end
+
+    it "should reject invalid email addresses" do
+      addresses = %w[user@foo,com user_at_foo.org example.user@foo.]
+      addresses.each do |address|
+        invalid_email_user = User.new(@attr.merge(:email => address))
+        invalid_email_user.should_not be_valid
+      end
+    end
   
-  it "should reject invalid email addresses" do
-     addresses = %w[user@foo,com user_at_foo.org first.last@foo.]
-     addresses.each do |address|
-       invalid_email_user = User.new(@attr.merge(:email => address))
-       invalid_email_user.should_not be_valid
-  end
   
   it "should reject duplicate email addresses" do
       # Put a user with given email address into the database.
@@ -54,15 +62,66 @@ describe User do
       user_with_duplicate_email.should_not be_valid
   end
   
+  describe "passwords" do
+    
+    before(:each) do
+      @user = User.new(@attr)
+    end
+    
+    it "should have a password attribute" do
+      @user.should respond_to(:password)
+    end
+    
+    it "should have a password confirmation attribute" do
+      @user.should respond_to(:password_confirmation)
+    end
+  end
+  
+  describe "password validations" do
+    it "should require a password" do
+      User.new(@attr.merge(:password => "", :password_confirmation => "")).
+       should_not be_valid
+     end
+     
+     it "should require a matching password confirmation" do
+       User.new(@attr.merge(:password_confirmation => "invalid")).
+       should_not be_valid
+   end
+   
+   it "should reject short passwords" do
+     short = "a" * 5
+     hash = @attr.merge(:password => short, :password_confirmation => short)
+     User.new(hash).should_not be_valid
+   end
+   
+   it "should reject long passwords" do
+      long = "a" * 41
+      hash = @attr.merge(:password => long, :password_confirmation => long)
+      User.new(hash).should_not be_valid
+    end
+  end
+
+  describe "password encryption" do
+    
+    before(:each) do
+      @user = User.create!(@attr)
+    end
+    
+    it "should have an encrypted password attribute" do
+      @user.should respond_to(:encrypted_password)
+    end
+  end
 end
+
 # == Schema Information
 #
 # Table name: users
 #
-#  id         :integer         not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime        not null
-#  updated_at :datetime        not null
+#  id                 :integer         not null, primary key
+#  name               :string(255)
+#  email              :string(255)
+#  created_at         :datetime        not null
+#  updated_at         :datetime        not null
+#  encrypted_password :string(255)
 #
 
